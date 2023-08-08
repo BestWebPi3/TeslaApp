1aaa5d7aa6104eedb77d01b25c9b5ae6c83ac712
BestWebPi3 <ircaj@seznam.cz>
2023-08-08 15:54:15 +0200

    Tesla-app

---
+++ app/index.js
@@ -0,0 +1,53 @@
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import car from "../assets/images/car.png";
+import menuOptions from "../assets/menuOptions";
+import MenuOption from "../components/MenuOption";
+import Controls from "../components/Controls";
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.title}>My model S</Text>
+          <Text style={styles.subtitle}>Parked</Text>
+        </View>
+        <FontAwesome name="user-circle" size={30} color="gray" />
+      </View>
+
+      <Image source={car} style={styles.image} resizeMode="contain" />
+
+      <Controls />
+
+      <FlatList data={menuOptions} renderItem={MenuOption} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#161818",
+  },
+  header: {
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 24,
+    color: "#eee",
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  subtitle: {
+    color: "gray",
+    fontWeight: "500",
+  },
+  image: {
+    width: "100%",
+    height: 300,
+  },
+});
